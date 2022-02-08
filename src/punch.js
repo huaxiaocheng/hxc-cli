83307ecab24f68a75cf77625bbffd84786ba5c9f
xiaocheng hua <xiaocheng.hua@pacteraedge.com>
2022-02-08 16:03:35 +0800

    feat: excel

--- src/punch.js
+++ src/punch.js
@@ -2,6 +2,7 @@ const fs = require('fs')
 const loading = require('loading-cli')
 const inquirer = require('inquirer')
 const log = require('npmlog')
+const excelJs = require('exceljs')
 
 module.exports = async (order) => {
     const today = new Date()
@@ -12,20 +13,51 @@ module.exports = async (order) => {
     if (order === undefined) {
         const load = loading('loading...').start()
         let data = await fs.readFileSync('d:/workspace/hxc-cli/data/punchTime.txt')
-        load.succeed('loading completed!')
+        load.succeed('load completed!')
 
         const automatic = data.toString().split('\r\n')[0].replace('T', ' ')
         const manual = data.toString().split('\r\n')[1]
         log.info('punch time - automatic report: ' + (automatic && automatic.substring(0, 10) === date ? automatic : ''))
         log.info('punch time - manual report   : ' + (manual && manual.substring(0, 10) === date ? manual : ''))
 
+        // 定时/关机前执行脚本：写入统计excel
+        const workbook = new excelJs.Workbook()
+        try {
+            await workbook.xlsx.readFile('d:/workspace/hxc-cli/data/punchTime.xlsx')
+            const worksheet = workbook.getWorksheet('Sheet1')
+            // 取消工作表保护
+            worksheet.unprotect()
+            let spaceRow = 0
+            worksheet.eachRow((row, index) => {
+                if (index > 1) {
+                    let excelDate = row._cells[0]._value.model.value
+                    if (excelDate === date) {
+                        spaceRow = index
+                        return
+                    }
+                }
+            })
+            if (spaceRow === 0) {
+                spaceRow = worksheet._rows.length + 1
+                worksheet.getCell(`A${spaceRow}`).value = date
+            }
+            worksheet.getCell(`B${spaceRow}`).value = automatic && automatic.substring(0, 10) === date ? automatic.substring(11, 16) : ''
+            worksheet.getCell(`C${spaceRow}`).value = manual && manual.substring(0, 10) === date ? manual.substring(11, 16) : ''
+            try {
+                await workbook.xlsx.writeFile('d:/workspace/hxc-cli/data/punchTime.xlsx')
+            } catch (e) {
+                log.error('update excel failed!')
+            }
+        } catch (e) {
+            log.error('load excel failed!')
+        }
     } else {
         if (!/^(?:(?:[0-2][0-3])|(?:[0-1][0-9]))[0-5][0-9]$/.test(order)) {
             console.log('time format e.g.: 0855')
         } else {
             const loadRead = loading('loading...').start()
             let data = fs.readFileSync('d:/workspace/hxc-cli/data/punchTime.txt')
-            loadRead.succeed('loading completed!')
+            loadRead.succeed('load completed!')
 
             const time =  order.substring(0, 2) + ':' + order.substring(2, 4)
 
@@ -56,7 +88,7 @@ module.exports = async (order) => {
                 newTxt[1] = date + ' ' + time
                 newTxt = newTxt.join('\r\n')
                 fs.writeFileSync('d:/workspace/hxc-cli/data/punchTime.txt', newTxt, 'utf8')
-                loadWrite.succeed('updating completed!')
+                loadWrite.succeed('update completed!')
 
                 log.info('updated punch time: ' + date + ' ' + time)
             }
